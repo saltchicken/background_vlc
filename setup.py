16b5501f7da24c7a42060383cfe5783e0a685eb1
saltchicken <john.eicher89@gmail.com>
2024-07-11 13:11:52 -0700

    Remove :main

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ setup(
 	],
 	entry_points ={ 
         'console_scripts': [ 
-            'background_vlc = background_vlc:main'
+            'background_vlc = background_vlc'
         ] 
     },
     python_requires='>=3.5',
